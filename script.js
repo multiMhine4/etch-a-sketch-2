e915d986be8d1c54e83c5d64b085a81fbbbb1dd6
Timothy Tesluk <timothy.tesluk@gmail.com>
2025-08-04 13:56:59 +0800

    Add hover event for each square

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ for (let row=0; row<16; row++) {
     for (let col=0; col<16; col++) {
         let newSquare = document.createElement("div");
         newSquare.classList.add("square");
+        newSquare.addEventListener("mouseover", (e) => {
+            e.target.style['background-color'] = 'yellow'; 
+        })
         newRow.appendChild(newSquare);
     }
     container.appendChild(newRow);
